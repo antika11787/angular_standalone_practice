1ee830c8fe20d1d8efc5bc47b16a9d482da84d6f
antika <antika.noor@bjitgroup.com>
2024-11-01 15:26:58 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
+import { ReversePipe } from './custom/reverse.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    HomeComponent,
+    CommonModule,
+    ReversePipe,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'newStandalone';
+  title = 'Angular 18';
+  subtitle = 'Angular practice project';
+  todayDate = new Date();
+  salary = 40000;
+  info = { name: 'Ant' };
+
+  isDisabled = false;
+
+  changeTitle() {
+    this.title = 'New Angular 18';
+  }
+
+  updateTitle(event: any) {
+    this.title = event.target.value;
+  }
 }
