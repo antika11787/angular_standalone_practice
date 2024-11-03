f09211ea9e4523f43d0485f735758281396b5db6
antika <antika.noor@bjitgroup.com>
2024-11-03 13:47:15 +0600

    ngular hooks

--- src/app/common/contact/contact.component.ts
+++ src/app/common/contact/contact.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
-export class ContactComponent {
 
+// ngOnChanges - handles changes in our inputs
+// ngOnInit - initialization of components and fetch data
+// ngDoCheck - reduces performance, runs after every onChange
+// ngAfterContentInit - runs after external content in current component
+// ngAfterContentChecked - handles changes in our projected content
+// ngAfterViewInit - runs after the view along with its children are initialized
+// ngAfterViewChecked - called after every check of the view along with its children
+export class ContactComponent
+  implements OnChanges, OnInit, DoCheck, AfterContentInit
+{
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('changes');
+  }
+
+  ngOnInit(): void {
+    console.log('init');
+  }
+
+  ngDoCheck(): void {
+    console.log('doCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('afterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('afterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('afterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('afterViewChecked');
+  }
 }
